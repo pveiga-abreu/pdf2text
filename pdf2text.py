69eb30f42aaa845e7bfd0269da02c3f6e8a60a16
pveiga-abreu <pveiga.abreu@gmail.com>
2021-08-26 17:53:25 -0300

    v0.0.3

--- pdf2text.py
+++ pdf2text.py
@@ -1,9 +1,10 @@
 from pdf2image import convert_from_bytes
+from datetime import datetime
+from io import BytesIO
 from PIL import Image
+import numpy as np
 import pytesseract
 import subprocess
-import tempfile
-import shutil
 import cv2
 import os
 
@@ -15,15 +16,7 @@ class PDF2Text:
         pdf_file: str -> path from de PDF file
         """
         self.__validate_tesseract()
-
-        self._pdf_path = pdf_file
-        self._tmpdir = tempfile.mkdtemp(
-            dir=os.path.dirname(os.path.realpath(__file__))
-            )
-        self._convert_to_image()
-
-    def __del__(self) -> None:
-        shutil.rmtree(self._tmpdir, ignore_errors=True)
+        self._convert_to_image(pdf_file)
 
     def __validate_tesseract(self) -> None:
         tess = subprocess.run(
@@ -32,73 +25,90 @@ class PDF2Text:
         ).stdout.decode('utf-8')
         if not tess: raise TesseractNotFoundError()
 
-    def _convert_to_image(self) -> None:
-        self._img_path = os.path.join(self._tmpdir, 'img.jpg')
-
+    def _convert_to_image(self, pdf_file: str) -> None:
         image = convert_from_bytes(
-            open(self._pdf_path, 'rb').read(),
+            open(pdf_file, 'rb').read(),
             dpi=350,
             single_file=True
         )[0]
         self.dimensions = image.size
-        image.save(self._img_path, 'JPEG')
 
-    def draw_lines(self, *args, thickness: int = 5, color: tuple = (0,0,255)) -> None:
+        with BytesIO() as f:
+            image.save(f,format="jpeg")
+            f.seek(0)
+            self.image = np.array(Image.open(f))[:, :, ::-1].copy()
+
+    def save_image(
+        self,
+        dir: str = './',
+        name: str = f'pdf2text_img_{datetime.now()}.jpeg'
+    ) -> None:
         """
-        args -> initial coordinate and final coordinate formatted like this ((X_tl, Y_tl), (X_br, Y_br)), 
-                you should pass all pair of coordinates you want to draw
-
-        thickness: int -> line thickness in pixels
-        color: tuple -> (Blue, Green, Red)
+        dir: str -> default './'
+        name: str -> default 'pdf2text_img_{datetime.now()}.jpeg'
         """
-        image = cv2.imread(self._img_path)
+        cv2.imwrite(
+            os.path.join(dir, name),
+            self.image
+        )
 
+    def draw_lines(
+        self,
+        *args,
+        thickness: int = 5,
+        color: tuple = (0,0,255)
+    ) -> None:
+        """
+        args : tuple -> top-left corner (X,Y), width and height formatted like this (X, Y, Width, Height)
+
+        thickness: int - default 5 -> line thickness in pixels
+        color: tuple - default (0,0,255) -> (Blue, Green, Red)
+        """
         for coordinates in args:
-            initial, final = coordinates
-            cv2.line(image, initial, final, color, thickness)
+            x, y, w, h = coordinates
+            cv2.line(self.image, (x,y), ((x+w),(y+h)), color, thickness)
 
-        cv2.imwrite(self._img_path, image)
-
-    def draw_rectangles(self, *args, thickness: int = 5, color: tuple = (0,0,255)) -> None:
+    def draw_rectangles(
+        self,
+        *args,
+        thickness: int = 5,
+        color: tuple = (0,0,255)
+    ) -> None:
         """
-        args -> top-left corner and bottom-right corner of rectangle formatted like this ((X_tl, Y_tl), (X_br, Y_br)), 
-                you should pass all pair of coordinates you want to draw
+        args : tuple -> top-left corner (X,Y), width and height formatted like this (X, Y, Width, Height)
 
-        thickness: int -> line thickness in pixels
-        color: tuple -> (Blue, Green, Red)
+        thickness: int - default 5 -> line thickness in pixels
+        color: tuple - default (0,0,255) -> (Blue, Green, Red)
         """
-        image = cv2.imread(self._img_path)
-
         for coordinates in args:
-            top_left, bottom_right = coordinates
-            cv2.rectangle(image, top_left, bottom_right, color, thickness)
+            x, y, w, h = coordinates
+            cv2.rectangle(self.image, (x,y), ((x+w),(y+h)), color, thickness)
 
-        cv2.imwrite(self._img_path, image)
-
-    def extract_text_from_location(self, coordinates: tuple, lang: str = 'eng') -> str:
+    def extract_text_from_location(
+        self,
+        coordinates: tuple,
+        lang: str = 'eng'
+    ) -> str:
         """
         coordinates : tuple -> top-left corner (X,Y), width and height formatted like this (X, Y, Width, Height) 
 
-        lang: str -> default 'eng'
+        lang: str - default 'eng' -> define tesseract language
 
         Returns text from the specified location
         """
-        image = cv2.imread(self._img_path)
-
         x,y,w,h = coordinates
-        chunk = image[y:y+h, x:x+w]
+        chunk = self.image[y:y+h, x:x+w]
         text = pytesseract.image_to_string(chunk, lang=lang)
 
         return text.strip()
 
-    def extract_all_text(self, lang: str = 'eng') -> str:
+    def extract_all_text(
+        self,
+        lang: str = 'eng'
+    ) -> str:
         """
-        lang: str -> default 'eng'
+        lang: str - default 'eng' -> define tesseract language
 
         Returns text from the pdf
         """
-        return pytesseract.image_to_string(
-                Image.open(self._img_path),
-                timeout=60,
-                lang=lang
-            )
+        return pytesseract.image_to_string(self.image, lang=lang)
